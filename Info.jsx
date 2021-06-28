0b03a57f039f41de14d7c8ab0ef2a28147d6604d
prachitidange <prachitidange@gmail.com>
2021-06-28 15:10:28 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Info() {
+  return (
+    <div class="note">
+      <h1> JavaScript And React.js</h1>
+      <p>A basic web dev React Js bootcamp</p>
+    </div>
+  );
+}
+export default Info;
